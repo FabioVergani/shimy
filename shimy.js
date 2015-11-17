5c9b6e021790c8d62dc28c6039cb2cfb414711f4
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-17 19:50:11 +0100

    Create shimy.js

---
+++ shimy.js
@@ -0,0 +1,6 @@
+/*
+'abbr','article','aside','audio','bdi','canvas','data','datalist','details','dialog','figcaption','figure','hgroup','main','mark','meter','nav','output','picture','progress','section','summary','template','time','video'
+*/
+if(d.documentMode && d.documentMode < 9){
+ for(var m=['footer','header'],i=m.length+0;i--;d.createElement(m[i]));	
+};
